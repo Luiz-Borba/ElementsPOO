6450b147ab2357153df91a260f017a24e106c789
Luiz <luis.p.borba@hotmail.com>
2023-04-12 17:05:21 -0300

    Classes melhor orientadas a objetos

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Ar ar = new Ar();
-        System.out.println(ar.getVelocidade());
+        Teste ar = new Teste();
+        System.out.println(ar.getAtaque());
 
     }
 
---
+++ Teste.java
@@ -0,0 +1,3 @@
+public class Teste extends Elementos{
+
+}
