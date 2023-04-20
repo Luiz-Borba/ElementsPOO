aae6cec19ec47432b5074fbd27ae64fbec5e34f3
Luiz <luis.p.borba@hotmail.com>
2023-04-20 15:46:45 -0300

    implementação de algumas funções menu

--- Menu.java
+++ Menu.java
@@ -1,6 +1,41 @@
+import java.util.Scanner;
+
 public class Menu {
+    Scanner input = new Scanner(System.in);
     public boolean menu(Agua agua,Fogo fogo,Terra terra,Ar ar){
-        //colocar o codigo menu aqui ja passei os os metodos como parametros
+        int opcaoIniciarGame;
+        int criatura;
+        int gamePlay;
+        System.out.println("1 - Iniciar novo jogo\n2 - Sair");
+        opcaoIniciarGame = input.nextInt();
+        if (opcaoIniciarGame == 1){
+            System.out.println("Escolha uma das Criaturas abaixo");
+            System.out.println("1 - StoneDev (terra)\n2 -  WaveNerd (água)\n3 - BurnCoder (fogo)\n4 - BreezeHacker (ar)");
+            criatura = input.nextInt();
+            System.out.println("1 - Inicar torneio\n2 - Sair do programa");
+            gamePlay = input.nextInt();
+
+            if (gamePlay == 1){
+                if (criatura == 1){
+                     //codigo
+                }
+                if (criatura == 2){
+                    //codigo
+                }
+                if (criatura == 3){
+                    //codigo
+                }
+                if (criatura == 4){
+                    //codigo
+                }
+                if (gamePlay == 2){
+                    System.out.println("Fechando Programa");
+                }
+            }
+
+        }
+
+
         return true;
     }
 
