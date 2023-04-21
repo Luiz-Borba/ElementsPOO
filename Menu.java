b5b049bee24ed1aea90cdc39b18300dbb14fcd4e
Luiz <luis.p.borba@hotmail.com>
2023-04-20 21:03:35 -0300

    criando o chat do primeiro menu

--- Menu.java
+++ Menu.java
@@ -9,6 +9,9 @@ public class Menu {
         int criatura;
         int gamePlay;
         int escolha;
+        double fator;
+        double dano;
+        double statusVida;
         System.out.println("== MENU PRINCIPAL ==");
         System.out.println("1 - Novo jogo");
         System.out.println("2 - Sair");
@@ -27,30 +30,43 @@ public class Menu {
                 if (criatura == 1){
                     int maisVeloz = vereficar.verificadorDeVelocidade(agua, fogo, ar, terra, 3);
                     if(maisVeloz == 2){
-                        System.out.println("teste");
                         System.out.println("Como você deseja atacar?");
                         System.out.println("1 - Ataque Elemental\n2 - Ataque Fisico");
                         int ataque = input.nextInt();
                         if(ataque == 1){
-                            System.out.println("Você está atacando WaveNerd");
-                            double fator = acao.descobrirFatorDePoder(terra.getTipoElemento(), agua.getTipoElemento());
-                            double dano = acao.calculoAtaqueElemental(terra.getPoder(), terra.getAtaque(), agua.getDefesa(), fator);
-                            System.out.println("Terra atacou Água e dano :" + dano);
+                            System.out.println("WaveNerd efeutou um ataque elemental!");
+                            fator = acao.descobrirFatorDePoder(terra.getTipoElemento(), agua.getTipoElemento());
+                            dano = acao.calculoAtaqueElemental(terra.getPoder(), terra.getAtaque(), agua.getDefesa(), fator);
                             System.out.println("vida atual da agua:" + agua.getVida());
-                            double statusVida = agua.getVida() - dano;
+                            System.out.println("Terra atacou Água e o dano foi de :" + dano);
+                            statusVida = agua.getVida() - dano;
                             agua.setVida((int) statusVida);
-                            System.out.println("vida da Água pós ataque: " + agua.getVida());
+                            System.out.println("vida da Água pós ataque : " + agua.getVida());
+                        }
+                        if(ataque == 2){
+                            System.out.println("WaveNerd efeutou um ataque fisico!");
+                            fator = acao.descobrirFatorDePoder(terra.getTipoElemento(), agua.getTipoElemento());
+                            dano = acao.calcaloAtaqueComun(terra.getPoder(), terra.getAtaque(), agua.getDefesa());
+                            System.out.println("vida atual da agua:" + agua.getVida());
+                            System.out.println("Terra atacou Água e o dano foi de :" + dano);
+                            statusVida = agua.getVida() - dano;
+                            agua.setVida((int) statusVida);
+                            System.out.println("vida da Água pós ataque : " + agua.getVida());
+
                         }
                     }
                     //codigo
                 }
                 if (criatura == 2){
                     //codigo
+                    System.out.println("ciratura 2");
                 }
                 if (criatura == 3){
                     //codigo
+                    System.out.println("criatura 3");
                 }
                 if (criatura == 4){
+                    System.out.println("Criatura 4");
                     //codigo
                 }
                 if (gamePlay == 2){
